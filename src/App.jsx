3d2446a8b60b401c1099e088df7a8acdb6600c04
Harikrishnan S <harikrishnan3522@gmail.com>
2025-08-23 21:51:17 +0530

    Dashboard added to user

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Login from "./pages/login";
 import { useState , useEffect } from "react";
 import AdminLogin from "./pages/Admin_login";
 import UserLogin from "./pages/User_login";
+import UserDashboard from "./pages/User_dashboard";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/admin" element={<AdminLogin />} />
       <Route path="/user" element={<UserLogin />} />
+      <Route path="/user-dashboard" element={<UserDashboard />} />
     </Routes>
   );
 } 
